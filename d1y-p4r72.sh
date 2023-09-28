3d1eddf47b769b92f0de2c4a9d5cf26ad64ae8cd
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-09-27 19:13:43 -0700

    Update d1y-p4r72.sh

--- d1y-p4r72.sh
+++ d1y-p4r72.sh
@@ -7,29 +7,13 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.177.100/g' package/base-files/files/bin/config_generate
 
-# Set password to PASSWORD
-# sed -i 's/root:::0:99999:7:::/root:$1$4xKZB45Q$w0CPT5M6vBWbYNmSWuxfU.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
-
 # Change ash to bash
 sed -i 's/ash/bash/g' package/base-files/files/etc/passwd
 
-# Change language=auto to zh_cn
-#sed -i 's/lang="auto"/lang="zh_cn"/g' package/emortal/default-settings/files/99-default-settings
-
 # Change luci list name
-#sed -i 's/"Argon 主题设置"/"主题设置"/g' feeds/luci/applications/luci-app-argon-config/po/zh_Hans/argon-config.po
-
-#sed -i 's/services/system/g' feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
-#sed -i 's/nas/services/g' feeds/luci/applications/luci-app-samba4/root/usr/share/luci/menu.d/luci-app-samba4.json
-
-# change some tips
-#sed -i 's/"终端"/"TTYD 终端"/g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
-
-#sed -i 's/START=18/START=99/g' feeds/packages/net/dnscrypt-proxy2/files/dnscrypt-proxy.init
-
-# DHCP
-#mkdir -p package/base-files/files/etc/dnsmasq.d
-#wget --no-check-certificate -O package/base-files/files/etc/dnsmasq.d/accelerated-domains.china.conf "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf"
+sed -i 's/"Argone 主题设置"/"主题设置"/g' feeds/kenzo/luci-app-argone-config/po/zh-cn/argone-config.po
+sed -i '/\t\trevert_dns/d' feeds/luci/applications/luci-app-turboacc/root/etc/init.d/turboacc
+sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po
 
 # Boost UDP
 echo '# optimize udp' >>package/base-files/files/etc/sysctl.d/10-default.conf
@@ -39,33 +23,9 @@ echo 'net.core.wmem_max=26214400' >>package/base-files/files/etc/sysctl.d/10-def
 echo 'net.core.wmem_default=26214400' >>package/base-files/files/etc/sysctl.d/10-default.conf
 echo 'net.core.netdev_max_backlog=2048' >>package/base-files/files/etc/sysctl.d/10-default.conf
 
-#5. Change luci list name
-sed -i 's/5e72e4/bc1142/g' feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
-sed -i 's/5e72e4/bc1142/g' feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/css/dark.css
-sed -i 's/483d8b/bc1142/g' feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
-sed -i 's/483d8b/bc1142/g' feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/css/dark.css
-
-rm -rf feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/favicon.ico
-wget --no-check-certificate -O feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/favicon.ico "https://raw.githubusercontent.com/0xACE8/r45pb3rry_p1_3b_1m/main/argon/favicon.ico"
-rm -rf feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/img/argon.svg
-wget --no-check-certificate -O feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/img/argon.svg "https://raw.githubusercontent.com/0xACE8/r45pb3rry_p1_3b_1m/main/argon/img/argon.svg"
-rm -rf feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-wget --no-check-certificate -O feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg "https://raw.githubusercontent.com/0xACE8/r45pb3rry_p1_3b_1m/main/argon/img/bg1.jpg"
-rm -rf feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/icon/*.png
-wget --no-check-certificate -O feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/icon/android-icon-192x192.png "https://raw.githubusercontent.com/0xACE8/r45pb3rry_p1_3b_1m/main/argon/icon/android-icon-192x192.png"
-wget --no-check-certificate -O feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/icon/apple-icon-60x60.png "https://raw.githubusercontent.com/0xACE8/r45pb3rry_p1_3b_1m/main/argon/icon/apple-icon-60x60.png"
-wget --no-check-certificate -O feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/icon/apple-icon-72x72.png "https://raw.githubusercontent.com/0xACE8/r45pb3rry_p1_3b_1m/main/argon/icon/apple-icon-72x72.png"
-wget --no-check-certificate -O feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/icon/apple-icon-144x144.png "https://raw.githubusercontent.com/0xACE8/r45pb3rry_p1_3b_1m/main/argon/icon/apple-icon-144x144.png"
-wget --no-check-certificate -O feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/icon/favicon-16x16.png "https://raw.githubusercontent.com/0xACE8/r45pb3rry_p1_3b_1m/main/argon/icon/favicon-16x16.png"
-wget --no-check-certificate -O feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/icon/favicon-32x32.png "https://raw.githubusercontent.com/0xACE8/r45pb3rry_p1_3b_1m/main/argon/icon/favicon-32x32.png"
-wget --no-check-certificate -O feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/icon/favicon-96x96.png "https://raw.githubusercontent.com/0xACE8/r45pb3rry_p1_3b_1m/main/argon/icon/favicon-96x96.png"
-wget --no-check-certificate -O feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/icon/ms-icon-144x144.png "https://raw.githubusercontent.com/0xACE8/r45pb3rry_p1_3b_1m/main/argon/icon/ms-icon-144x144.png"
-
-# Change to my banner
-#sudo rm package/base-files/files/etc/banner
-#wget --no-check-certificate https://raw.githubusercontent.com/0xACE8/r45pb3rry_p1_3b_1m/main/banner -O package/base-files/files/etc/banner
-
 # upgrade config
-wget --no-check-certificate https://raw.githubusercontent.com/0xACE8/r45pb3rry_p1_3b_1m/main/99-init-settings -O package/base-files/files/etc/uci-defaults/99-init-settings
+wget --no-check-certificate https://raw.githubusercontent.com/0xACE8/y0uku_yk-l1/main/zzz-default-settings -O package/lean/default-settings/files/zzz-default-settings
+
+wget --no-check-certificate https://raw.githubusercontent.com/0xACE8/y0uku_yk-l1/main/99-init-settings -O package/base-files/files/etc/uci-defaults/99-init-settings
 
 echo "diy-part2.sh is done."
